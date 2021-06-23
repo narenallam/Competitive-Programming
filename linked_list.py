74d89ea7741fb39cb9929f4316fa0ad9e38e4f51
Naren Allam <naallam@elacarte.com>
2021-06-23 09:16:52 +0530

    pop() has been added

--- linked_list.py
+++ linked_list.py
@@ -26,10 +26,40 @@ class LinkedList:
         self.count += 1
 
     def insert(self, _data, pos):
-        pass
+        new_node = Node(_data)
+        if pos <= 0:
+            new_node.next = self.head
+            self.head = new_node
+        elif pos >= self.count:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            tmp = self.head
+            for _ in range(pos-1):
+                tmp = tmp.next
+            new_node.next = tmp.next
+            tmp.next = new_node
 
     def pop(self, pos=None):
-        pass
+        if not pos:
+            _ptr = self.head
+            for _ in range(self.count-2):
+                _ptr = _ptr.next
+            tmp = _ptr.next
+            _ptr.next = None
+            self.tail = _ptr
+
+        elif 0 < pos < self.count:
+            _ptr = self.head
+            for _ in range(pos-1):
+                _ptr = _ptr.next
+            tmp = _ptr.next
+            _ptr.next = None
+
+        _data = tmp.data
+        del tmp
+        self.count -= 1
+        return _data
 
     def remove(self, _data):
         pass
@@ -59,5 +89,8 @@ class LinkedList:
 
 
 if __name__ == '__main__':
-    ll = LinkedList([5, 6, 2, 4, 8, 1, 9])
+    ll = LinkedList([5, 6, 2, 4,  8, 1, 9])
+    # ll.insert(66, 3)
+    print(ll.pop())
+    print(ll.pop())
     print(ll)
